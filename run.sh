1188fb97d691aa70d7fd7778ed1a3f0748799ad9
Владимир <slider@Mac-mini-Vladimir.local>
2023-11-23 20:41:07 +0700

    ext autorun plist

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 10
-date >>runme.txt
-echo running camilladsp >>runme.txt
+date >>logs/camilladsp.log
+echo running camilladsp >>logs/camilladsp.log
 while TRUE
 do
  ./bin/camilladsp ./conf/camilladsp.yml -p 1234 --logfile ./logs/camilladsp.log
