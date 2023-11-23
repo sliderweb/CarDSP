a165f4acad3dbec590f3663ed4a123430409f160
Владимир <slider@Mac-mini-Vladimir.local>
2023-11-24 02:46:45 +0700

    Debug logging

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ date >>logs/camilladsp.log
 echo running camilladsp >>logs/camilladsp.log
 while TRUE
 do
- ./bin/camilladsp ./conf/camilladsp.yml -p 1234 --logfile ./logs/camilladsp.log
+ ./bin/camilladsp ./conf/camilladsp.yml -g-40 -p 1234 --logfile ./logs/camilladsp.log
 done
